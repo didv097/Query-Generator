c23dca9bee7a027ff742423f3277432fb203cfef
bran0 <ycj@Macintosh.local>
2019-07-30 10:45:31 -0500

    commit

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Button, Box} from '@material-ui/core';
 
 export default function NotFoundPage() {
   return (
-    <div>
+    <Box m={2}>
       <h1>404 Page not found</h1>
-    </div>
+      <Button href="/" variant="outlined">
+        <i className="material-icons">home</i>
+        Go to home
+      </Button>
+    </Box>
   )
 }
