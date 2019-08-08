ab1a1a9b63aeecedd7cdc47b3566d701d01742b0
bran0 <ycj@Macintosh.local>
2019-08-08 09:41:50 -0500

    update

--- src/index.js
+++ src/index.js
@@ -10,17 +10,18 @@ import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:4000/graphql'
+	uri: 'http://localhost:4000/graphql'
 })
 
 const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
+	link: httpLink,
+	cache: new InMemoryCache()
 })
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>, document.getElementById('root'));
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>, document.getElementById('root')
+);
 
 serviceWorker.unregister();
